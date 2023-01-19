5cf2db1cf90e311ae245846c58ca03ebd8eee210
lucasXIIIoffin <lucasXIIIoffin@gmail.com>
2023-01-19 20:32:35 -0300

    Conteudo da aula

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-//MM-DD
-const data = {
-  run: ["01-01", "01-02", "01-03"],
+button.addEventListener("click", add)
+// salvar os dias 
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❗")
+    return
+  }
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
+//salvar o dia
+function save(){
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
